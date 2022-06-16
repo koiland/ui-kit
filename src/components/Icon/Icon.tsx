b9becadcd8dbe599aaa484d8ef64966bc3718454
Alex Eliseev <oielisieiev@shopmonkey.io>
2022-06-16 02:19:20 +0200

    [master] – added React.memo for Icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -6,14 +6,10 @@ import { TIconProps } from './Icon.types';
 /* @Styled */
 import { IconStyled } from './Icon.styled';
 
-export const Icon = <P extends unknown>({
-  glyph: Glyph,
-  size = 'md',
-  color,
-  style,
-  props = {} as P,
-}: TIconProps<P>): JSX.Element => (
-  <IconStyled size={size} color={color}>
-    <Glyph style={style} {...props} />
-  </IconStyled>
+export const Icon = React.memo(
+  <P extends unknown>({ glyph: Glyph, size = 'md', color, style, props = {} as P }: TIconProps<P>): JSX.Element => (
+    <IconStyled size={size} color={color}>
+      <Glyph style={style} {...props} />
+    </IconStyled>
+  )
 );
